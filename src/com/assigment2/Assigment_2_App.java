8e25da1c206b3c59a1745af4fe703f059b7361d7
Nathan Center <nathanjcenter@gmail.com>
2022-08-04 15:00:17 -0500

    first commit

---
+++ src/com/assigment2/Assigment_2_App.java
@@ -0,0 +1,11 @@
+package com.assigment2;
+
+public class Assigment_2_App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+
+	}
+
+}
